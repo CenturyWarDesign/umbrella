2e2af6cba23fd66a95051a7eab13cc6369a38534
wanbin <wanbin@sevenga.com>
2015-07-21 15:21:07 +0800

    微信验证

---
+++ protected/controllers/WxController.php
@@ -0,0 +1,108 @@
+<?php
+
+define("TOKEN", "dLneoDa897Dn2Ac");
+
+class WxController extends Controller
+{
+	public function actionIndex()
+	{
+		if(Yii::app()->params['wxvaild']){
+			$this->valid();
+		}else{
+			$this->responseMsg();
+		}
+	}
+	
+	
+	public function valid()
+	{
+		$echoStr = $_GET["echostr"];
+	
+		//valid signature , option
+		if($this->checkSignature()){
+			echo $echoStr;
+			exit;
+		}
+	}
+	
+	public function responseMsg()
+	{
+		//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+	
+		//extract post data
+		if (!empty($postStr)){
+	
+			$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+			$fromUsername = $postObj->FromUserName;
+			$toUsername = $postObj->ToUserName;
+			$keyword = trim($postObj->Content);
+			$time = time();
+			$textTpl = "<xml>
+			<ToUserName><![CDATA[%s]]></ToUserName>
+			<FromUserName><![CDATA[%s]]></FromUserName>
+			<CreateTime>%s</CreateTime>
+			<MsgType><![CDATA[%s]]></MsgType>
+			<Content><![CDATA[%s]]></Content>
+			<FuncFlag>0</FuncFlag>
+			</xml>";
+			if(!empty( $keyword ))
+			{
+				$msgType = "text";
+				$contentStr = "Welcome to wechat world!";
+				$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+				echo $resultStr;
+			}else{
+				echo "Input something...";
+			}
+	
+		}else {
+			echo "";
+			exit;
+		}
+	}
+	
+	private function checkSignature()
+	{
+		$signature = $_GET["signature"];
+		$timestamp = $_GET["timestamp"];
+		$nonce = $_GET["nonce"];
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr, SORT_STRING);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+	
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
