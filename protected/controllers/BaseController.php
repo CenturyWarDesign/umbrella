f24fe4c3750e445e8d837b1ac5f970500f406445
wanbin <wanbin@sevenga.com>
2015-07-24 11:19:34 +0800

    添加wxjs测试

--- protected/controllers/BaseController.php
+++ protected/controllers/BaseController.php
@@ -1,11 +1,13 @@
 <?php
 class BaseController extends Controller
 {
+	public $signPackage='';
 	public function init()
 	{
 		Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/css/jquery-2.1.4.min.js');
 		Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl.'/css/bootstrap/css/bootstrap.css');
 		Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/css/bootstrap/js/bootstrap.min.js');
+		$this->signPackage=$this->getSignPackage();
 	}
 	
 	private function createNonceStr($length = 16) {
@@ -15,5 +17,30 @@ class BaseController extends Controller
 			$str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
 		}
 		return $str;
+	}
+	public function getSignPackage() {
+		$jsapiTicket = WX::actionTicketToken();
+	
+		// 注意 URL 一定要动态获取，不能 hardcode.
+		$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+		$url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+	
+		$timestamp = time();
+		$nonceStr = $this->createNonceStr();
+	
+		// 这里参数的顺序要按照 key 值 ASCII 码升序排序
+		$string = "jsapi_ticket=$jsapiTicket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
+	
+		$signature = sha1($string);
+	
+		$signPackage = array(
+				"appId"     => Yii::app()->params['appid'],
+				"nonceStr"  => $nonceStr,
+				"timestamp" => $timestamp,
+				"url"       => $url,
+				"signature" => $signature,
+				"rawString" => $string
+		);
+		return $signPackage;
 	}
 }
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -17,14 +17,23 @@
 <script>
 wx.config({
     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    appId: '', // 必填，公众号的唯一标识
-    timestamp: , // 必填，生成签名的时间戳
-    nonceStr: '', // 必填，生成签名的随机串
-    signature: '',// 必填，签名，见附录1
-    jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    appId: '<?php echo $this->signPackage["appId"];?>',
+    timestamp: <?php echo $this->signPackage["timestamp"];?>,
+    nonceStr: '<?php echo $this->signPackage["nonceStr"];?>',
+    signature: '<?php echo $this->signPackage["signature"];?>',
+    jsApiList: ['getLocation'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 });
 wx.ready(function () {
     // 在这里调用 API
+	wx.getLocation({
+	    type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+	    success: function (res) {
+	        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+	        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+	        var speed = res.speed; // 速度，以米/每秒计
+	        var accuracy = res.accuracy; // 位置精度
+	    }
+	});
 });
 </script>
 </body>
