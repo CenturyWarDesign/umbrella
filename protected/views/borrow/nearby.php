48e797e12127aa7392aea65b345a33dcf4c94d3b
wanbin <497442643@qq.com>
2015-07-25 16:03:54 +0800

    经纬度写反了

--- protected/views/borrow/nearby.php
+++ protected/views/borrow/nearby.php
@@ -30,7 +30,7 @@ wx.ready(function(){
 	        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
 	        var speed = res.speed; // 速度，以米/每秒计
 	        var accuracy = res.accuracy; // 位置精度
-	        map.centerAndZoom(new BMap.Point(latitude, longitude), 14);  
+	        map.centerAndZoom(new BMap.Point(longitude,latitude), 14);  
 	    }
 	});
 });
