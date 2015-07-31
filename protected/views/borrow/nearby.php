51ae6e7921cbc78bfeb978cb9f183016c2fa8278
wanbin <497442643@qq.com>
2015-07-31 14:35:53 +0800

    返回到当前位置

--- protected/views/borrow/nearby.php
+++ protected/views/borrow/nearby.php
@@ -1,5 +1,5 @@
 <div id='error'></div>
-<div id="l-map" style='width:100%;height:100%'></div>
+<div id="l-map" style='width: 100%; height: 100%'></div>
 <script>
 $("html").css("width","100%");
 $("html").css("height","100%");
@@ -18,7 +18,9 @@ var scaleControl=new BMap.ScaleControl();
 map.addControl(scaleControl);//添加比例尺控件  
 map.centerAndZoom(new BMap.Point(<?php echo Yii::app()->params['map_center_latitude']?>, <?php echo Yii::app()->params['map_center_longitude']?>), 16); 
 
-wx.ready(function(){
+
+
+function panToNear(){
 	wx.getLocation({
 	    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
 	    success: function (res) {
@@ -29,23 +31,8 @@ wx.ready(function(){
 	        $.post("getBaiduPoint",{from:1,to:3,x:longitude,y:latitude},function(result){
 	        	map.panTo(new BMap.Point(result.x,result.y), 14);  
 	        },"json");
-	        getNearby(latitude,longitude);
 	    }
 	});
-});
-$(document).ready(function(){
-	var bound=map.getBounds();
-	getBound(bound._swLat,bound._swLng,bound._neLat,bound._neLng);
-});
-
-function getNearby(latitude,longitude){
-	$.get("nearbysearch",function(result){
-		for(var key in result.contents){
-			var value=result.contents[key];
-			var marker = new BMap.Marker(new BMap.Point(value.location[0], value.location[1]));        // 创建标注      
-			map.addOverlay(marker);
-        }    
-	},"json");
 }
 
 function getBound(swlat,swlng,nelat,nelng){
@@ -78,7 +65,44 @@ map.addEventListener("moveend", function(){
 });
 
 
+//定义一个控件类，即function      
+function ZoomControl(){      
+    // 设置默认停靠位置和偏移量    
+    this.defaultAnchor = BMAP_ANCHOR_BOTTOM_LEFT;      
+    this.defaultOffset = new BMap.Size(10, 50);      
+}      
+// 通过JavaScript的prototype属性继承于BMap.Control     
+ZoomControl.prototype = new BMap.Control();   
+//自定义控件必须实现initialize方法，并且将控件的DOM元素返回     
+//在本方法中创建个div元素作为控件的容器，并将其添加到地图容器中     
+ZoomControl.prototype.initialize = function(map){      
+	//创建一个DOM元素     
+	var div = document.createElement("div");      
+	//添加文字说明      
+	div.appendChild(document.createTextNode("当前"));      
+	//设置样式      
+	div.style.cursor = "pointer";      
+	div.style.border = "1px solid gray";      
+	div.style.backgroundColor = "white";      
+	//绑定事件，点击一次放大两级      
+	div.onclick = function(e){    
+		panToNear();
+	}      
+	//添加DOM元素到地图中     
+	map.getContainer().appendChild(div);    
+	return div;    
+}  
 
+//创建控件实例      
+var myZoomCtrl = new ZoomControl();      
+// 添加到地图当中      
+map.addControl(myZoomCtrl); 
+
+
+
+wx.ready(function(){
+	panToNear();
+});
 </script>
 
 
